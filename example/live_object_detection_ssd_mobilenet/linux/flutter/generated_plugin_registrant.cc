94b5cb1151cdb09f9f7a9e1df5b93c793ef6c599
Joel Winarske <joel.winarske@gmail.com>
2025-08-29 12:35:09 -0700

    live_object_detection_ssd_mobilenet
    -add camera_linux reference
    
    Signed-off-by: Joel Winarske <joel.winarske@gmail.com>

--- example/live_object_detection_ssd_mobilenet/linux/flutter/generated_plugin_registrant.cc
+++ example/live_object_detection_ssd_mobilenet/linux/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,13 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <camera_linux/camera_linux.h>
 #include <file_selector_linux/file_selector_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) camera_linux_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "CameraLinux");
+  camera_linux_register_with_registrar(camera_linux_registrar);
   g_autoptr(FlPluginRegistrar) file_selector_linux_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "FileSelectorPlugin");
   file_selector_plugin_register_with_registrar(file_selector_linux_registrar);
--- example/live_object_detection_ssd_mobilenet/linux/flutter/generated_plugins.cmake
+++ example/live_object_detection_ssd_mobilenet/linux/flutter/generated_plugins.cmake
@@ -3,6 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  camera_linux
   file_selector_linux
 )
 
